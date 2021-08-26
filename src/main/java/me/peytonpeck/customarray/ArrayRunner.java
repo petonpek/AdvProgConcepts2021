f6b48859d7345a0a8b80be58ec4d9159df471802
Sizzle <57070735+SizzleMcGrizzle@users.noreply.github.com>
2021-08-26 11:01:54 -0500

    More testing

--- src/main/java/me/peytonpeck/customarray/ArrayRunner.java
+++ src/main/java/me/peytonpeck/customarray/ArrayRunner.java
@@ -1,9 +1,12 @@
 package me.peytonpeck.customarray;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class ArrayRunner {
 
     public static void main(String[] args) {
-        int testLength = 300000;
+        int testLength = 200000;
 
         Array<Integer> array = new Array<>(3);
 
@@ -18,8 +21,19 @@ public class ArrayRunner {
         for (int i = 0; i < testLength; i++)
             oldArray.insert(i);
         long end2 = System.nanoTime();
+        
+        List<Integer> list = new LinkedList<>();
+
+        long start3 = System.nanoTime();
+        for (int i = 0; i < testLength; i++) {
+            if (i % (testLength/10) == 0)
+                System.out.println(i);
+            list.add(i);
+        }
+        long end3 = System.nanoTime();
 
         System.out.println((double)(end-start)/(double)1000000000 + "seconds (new)");
         System.out.println((double)(end2-start2)/(double)1000000000 + "seconds (old)");
+        System.out.println((double)(end3-start3)/(double)1000000000 + "seconds (linked)");
     }
 }
