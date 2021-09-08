9fc9a852f7186e01a8e2d57ff7efbcf685b06475
Sizzle <notsizzle@gmail.com>
2021-09-08 10:59:00 -0500

    More fun

--- src/main/java/me/p3074098/linkedlist/FunLinkedList.java
+++ src/main/java/me/p3074098/linkedlist/FunLinkedList.java
@@ -1,16 +1,10 @@
 package me.p3074098.linkedlist;
 
-<<<<<<< HEAD
-import java.util.Iterator;
-import java.util.LinkedList;
 
-=======
->>>>>>> 6a57e063d1632c546075f6abec662a647c3d4f04
 public class FunLinkedList<T> {
 
     private Node<T> head;
     private Node<T> tail;
-<<<<<<< HEAD
     private int count = 0;
 
     public void insert(T object) {
@@ -35,14 +29,14 @@ public class FunLinkedList<T> {
         }
 
         Node<T> next = head;
-        for (int i = 0; i < index-1; i++)
+        for (int i = 0; i < index - 1; i++)
             next = next.getNext();
 
         Node<T> replaced = next.getNext();
 
         next.setNext(node);
         node.setNext(replaced);
-=======
+    }
 
     public FunLinkedList() {
 
@@ -58,12 +52,10 @@ public class FunLinkedList<T> {
         while (next.getNext() != null) {
             next = next.getNext();
         }
->>>>>>> 6a57e063d1632c546075f6abec662a647c3d4f04
     }
 
     public void remove(int index) {
 
-<<<<<<< HEAD
         if (index < 0 || index >= count)
             throw new IllegalArgumentException("You must enter an index that is within the bounds of the size of the list");
 
@@ -102,28 +94,5 @@ public class FunLinkedList<T> {
 
     public Node<T> getTail() {
         return tail;
-=======
-    }
-
-    private static class Node<T> {
-        private final T data;
-        private Node<T> next;
-
-        public Node(T data) {
-            this.data = data;
-        }
-
-        public Node<T> getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
-
-        public T getData() {
-            return data;
-        }
->>>>>>> 6a57e063d1632c546075f6abec662a647c3d4f04
     }
 }
--- src/main/java/me/p3074098/linkedlist/NodeFun.java
+++ src/main/java/me/p3074098/linkedlist/NodeFun.java
@@ -3,46 +3,45 @@ package me.p3074098.linkedlist;
 public class NodeFun {
 
     public static void main(String[] args) {
-        Node list = new Node(1);
-        Node list2 = new Node(2);
-        list2.next = new Node(3);
-        list2.next.next = new Node(4);
+        FunNode list = new FunNode(1);
+        list.next = new FunNode(2);
+        list.next.next = new FunNode(3);
+        list.next.next.next = new FunNode(4);
 
         System.out.println("Printing nodes...");
         printNode(list);
-        printNode(list2);
 
-        list.next = list2;
-        list2 = list.next.next;
-        list2.next = null;
+        FunNode list2 = list;
+        list = list.next.next.next;
+        list.next = list2.next;
+        list2.next.next.next = list2;
+        list2 = list2.next.next;
+        list2.next.next = null;
+        list.next.next = null;
 
         System.out.println("Testing (1)...");
         printNode(list);
         printNode(list2);
 
-        list.next.next.next = list;
-        list = list.next.next;
-        list.next.next.next.next = null;
-
-        System.out.println("Printing again!");
-        printNode(list);
-        printNode(list2);
     }
 
-    private static void printNode(Node node) {
-        Node temp = node;
+    private static void printNode(FunNode node) {
+        FunNode temp = node;
         while (temp != null) {
-            System.out.println(temp.data);
+            System.out.print(temp.data);
             temp = temp.next;
+            if (temp != null)
+                System.out.print(" --> ");
         }
+        System.out.println();
     }
 
-    private static class Node {
+    private static class FunNode {
 
         public int data;
-        public Node next;
+        public FunNode next;
 
-        public Node(int data) {
+        public FunNode(int data) {
             this.data = data;
         }
     }
